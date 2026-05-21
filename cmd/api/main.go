e25e0a05686c3b80c4c1a806fc66e4fb7ae6f726
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-05-21 10:55:31 +0600

    Wire API server to PostgreSQL training service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+    "context"
     "log/slog"
     "os"
 
     "github.com/NursultanKoshoev11/VRTrainingServer/internal/config"
+    "github.com/NursultanKoshoev11/VRTrainingServer/internal/db"
     "github.com/NursultanKoshoev11/VRTrainingServer/internal/httpserver"
+    "github.com/NursultanKoshoev11/VRTrainingServer/internal/training"
 )
 
 func main() {
     cfg := config.Load()
     logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: cfg.LogLevel()}))
 
-    srv := httpserver.New(cfg, logger)
+    database, err := db.Connect(context.Background(), cfg.DatabaseURL)
+    if err != nil {
+        logger.Error("database connection failed", "error", err)
+        os.Exit(1)
+    }
+    defer database.Close()
+
+    trainingRepo := training.NewPostgresRepository(database.Pool)
+    trainingService := training.NewService(trainingRepo)
+
+    srv := httpserver.New(cfg, logger, trainingService)
 
     logger.Info("starting VRTrainingServer", "address", cfg.HTTPAddress())
     if err := srv.Start(); err != nil {
